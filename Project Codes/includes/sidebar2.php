9aca8bb3884227e8f38638a54393900c39993aa9
ksheeraj kandra <kandra.ksheeraj@gmail.com>
2021-07-12 21:20:50 +0530

    Add files via upload

---
+++ Project Codes/includes/sidebar2.php
@@ -0,0 +1,40 @@
+<aside id="left-panel" class="left-panel">
+        <nav class="navbar navbar-expand-sm navbar-default">
+            <div id="main-menu" class="main-menu collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active">
+                        <a href="user-dashboard.php"><i class="menu-icon fa fa-laptop"></i>Dashboard </a>
+                    </li>
+                   
+                   
+                    <li class="menu-item-has-children dropdown">
+                        
+                        <ul class="sub-menu children dropdown-menu">
+                    
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="add-vehicle2.php"> <i class="menu-icon ti-email"></i>Booking</a>
+                    </li>
+                    <li class="menu-item-has-children dropdown">
+                        <ul class="sub-menu children dropdown-menu">
+                          
+                           
+                        </li>
+
+                        </ul>
+                    </li>
+                    <li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-th"></i>Records</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><i class="menu-icon fa fa-th"></i><a href="bwdates-report-ds2.php">Between Dates Reports</a></li>
+                           
+                        </ul>
+                    </li>
+<li>
+                        
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </nav>
+    </aside>
